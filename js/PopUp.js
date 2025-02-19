0f3ff7de79b55321c94b02e1cef7b3ff484b36a3
Liubava <liubava.k@bk.ru>
2025-02-19 11:55:01 +0300

    edited src path for cards

--- js/PopUp.js
+++ js/PopUp.js
@@ -1,8 +1,9 @@
 class PopUp {
   constructor(path, title, price, description, learn_more) {
-    this.el = document.createElement("div");
-    this.el.classList.add("modal");
-    this.src = `/solar-system/images/${path}`;
+    this.el = document.createElement('div');
+    this.el.classList.add('modal');
+    // this.src = `/solar-system/images/${path}`;
+    this.src = `${path}`;
     this.title = title;
     this.price = price;
     this.description = description;
@@ -19,17 +20,17 @@ class PopUp {
     <p>${this.description}</p>
     </div>
     `;
-    document.querySelector(".dialog").addEventListener("click", function (e) {
-      console.log("iamcalled");
-      if (e.target.classList.contains("dialog")) {
-        const dialog = document.querySelector(".dialog");
+    document.querySelector('.dialog').addEventListener('click', function (e) {
+      console.log('iamcalled');
+      if (e.target.classList.contains('dialog')) {
+        const dialog = document.querySelector('.dialog');
         // если объект содержит такой класс,то выполнять код ниже
         dialog.style.opacity = 0;
-        dialog.style.bottom = "-200%";
+        dialog.style.bottom = '-200%';
         dialog.style.zIndex = 0;
         //ниже проверяем, есть ли у диалога модалка и уже потом
         //удаляем первый элемент , так как у нас FIFO - первый пришел - первый ушел, соответсвенно, когда убираем модалку - удаляем первого и единстенного ребенка внутри, чтобы не было списка при открытии нового окошка
-        const modal = document.querySelector(".modal");
+        const modal = document.querySelector('.modal');
         if (dialog.contains(modal)) {
           dialog.removeChild(modal);
         }
@@ -41,9 +42,9 @@ class PopUp {
     this.render();
   }
   render() {
-    document.querySelector(".dialog").style.opacity = 1;
-    document.querySelector(".dialog").style.bottom = 0;
-    document.querySelector(".dialog").style.zIndex = 3;
-    document.querySelector(".dialog").appendChild(this.el);
+    document.querySelector('.dialog').style.opacity = 1;
+    document.querySelector('.dialog').style.bottom = 0;
+    document.querySelector('.dialog').style.zIndex = 3;
+    document.querySelector('.dialog').appendChild(this.el);
   }
 }
